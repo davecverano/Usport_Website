4ea94d72b3298dafc413db3ef197968c5f0c9d57
davecverano <davecverano@gmail.com>
2022-08-28 17:48:58 -0400

    add environmental variables in env.yaml file

--- server/main.py
+++ server/main.py
@@ -13,14 +13,10 @@ from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 from dotenv import load_dotenv
 
-load_dotenv()
+
 app = FastAPI()
 expiry_time = 10
 
-bucket_name = os.getenv('bucket_name')
-key = os.getenv('key')
-salt = os.getenv('salt')
-service_account_filename = os.getenv('service_account_filename')
 
 
 if os.getenv('GAE_ENV', '').startswith('standard'):
@@ -31,16 +27,17 @@ if os.getenv('GAE_ENV', '').startswith('standard'):
     ]
 else:
     # localhost
-    os.environ["DATASTORE_DATASET"] = "test"
-    os.environ["DATASTORE_EMULATOR_HOST"] = "localhost:8081"
-    os.environ["DATASTORE_EMULATOR_HOST_PATH"] = "localhost:8081/datastore"
-    os.environ["DATASTORE_HOST"] = "http://localhost:8081"
-    os.environ["DATASTORE_PROJECT_ID"] = "test"
+    load_dotenv()
     db = datastore.Client(project="test")
     origins = [
     "https://usport.club",
     "http://localhost:3000/",
     ]
+
+bucket_name = os.getenv('bucket_name')
+key = os.getenv('key')
+salt = os.getenv('salt')
+service_account_filename = os.getenv('service_account_filename')
 
 
 
--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import './index.css';
 import App from './App';
 import axios from 'axios';
 
-//axios.defaults.baseURL = 'https://server-dot-weighty-archive-270701.ue.r.appspot.com/';
+axios.defaults.baseURL = 'https://server-dot-weighty-archive-270701.ue.r.appspot.com/';
 //axios.defaults.baseURL = 'http://127.0.0.1:8000';
 
 
