3985fa697990ad1c7c375231dd96ace1fca5ff83
davecverano <davecverano@gmail.com>
2022-08-29 09:54:26 -0400

    update CORS for website

--- server/main.py
+++ server/main.py
@@ -18,16 +18,22 @@ app = FastAPI()
 expiry_time = 10
 
 
-
 if os.getenv('GAE_ENV', '').startswith('standard'):
     #production
     db = datastore.Client()
     origins = [
-    "https://usport.club"
+    "https://usport.club",
+    "https://usport.club/home",
+    "https://usport.club/schedule"
     ]
 else:
     # localhost
     load_dotenv()
+    os.environ["DATASTORE_DATASET"] = "test"
+    os.environ["DATASTORE_EMULATOR_HOST"] = "localhost:8081"
+    os.environ["DATASTORE_EMULATOR_HOST_PATH"] = "localhost:8081/datastore"
+    os.environ["DATASTORE_HOST"] = "http://localhost:8081"
+    os.environ["DATASTORE_PROJECT_ID"] = "test"
     db = datastore.Client(project="test")
     origins = [
     "https://usport.club",
